722ad4b7b8e5f48f051217265bcd04185f512549
AjayCharit-K.V <kodakallavenkataajaycharit25cs@rnsit.ac.in>
2026-01-28 21:18:18 +0530

    Added Word2Vec and removed Tfid

--- ai-projects/Spam.py
+++ ai-projects/Spam.py
@@ -5,7 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score,confusion_matrix,f1_score,precision_score,recall_score
-import gensim.models.word2vec
+import gensim.models.word2vec as Word2Vec
+
 
 import nltk
 from nltk.corpus import stopwords
@@ -27,6 +28,24 @@ def preprocess_text(text):
   tokens = [word for word in tokens if word not in stop_words]
   return ' '.join(tokens)  
 
+def tokenize_text(text):
+  text = text.lower()
+  text = "".join([char for char in text if char not in string.punctuation])
+  tokens = text.split()
+  stop_words = set(stopwords.words('english'))
+  tokens = [word for word in tokens if word not in stop_words]
+  return tokens
+def get_avg_word2vec(tokens,model,vector_size):
+  word_vect = []
+  for token in tokens:
+    if token in model.wv:
+      word_vect.append(model.wv[token])
+  if word_vect:
+    return np.mean(word_vect , axis=0)
+  else:
+    return np.zeros(vector_size)
+  
+
 script_dir = pathlib.Path(__file__).parent.resolve()
 csv_path = script_dir / 'SMSSpamCollection.csv'
 df = pd.read_csv(csv_path,sep= "\t",header = None,names=["label","message"],encoding='latin-1')
@@ -35,32 +54,13 @@ df['message'] = df["message"].apply(preprocess_text)
 
 X = df["message"] 
 y = df["label"]
-X_train , X_test , y_train , y_test = train_test_split(X,y,test_size= 0.2,random_state=42)
+X_train_raw , X_test_raw , y_train , y_test = train_test_split(X,y,test_size= 0.2,random_state=42)
 
-tf = TfidfVectorizer()
-tf_train = tf.fit_transform(X_train)
-tf_test = tf.transform(X_test)
+X_train_tokens = X_train_raw.apply(tokenize_text)
+X_test_tokens = X_test_raw.apply(tokenize_text)
 model = MultinomialNB()
-model.fit(tf_train,y_train)
-
-y_pred_train = model.predict(tf_train)
-train_accuracy = accuracy_score(y_train, y_pred_train) 
-train_preci = precision_score(y_train,y_pred_train)
-train_recall = recall_score(y_train,y_pred_train)
-train_f1 = f1_score(y_train,y_pred_train)
-print(f"Train Accuracy : {train_accuracy:.3f}")
-print("Training Confusion Matrix:\n", confusion_matrix(y_train, y_pred_train))
-print("Training Precision Score:\n", train_preci)
-print("Training Recall Score:\n", train_recall)
-print("Training F1-Score:\n", train_f1)
-
-y_pred_test = model.predict(tf_test)
-test_acc = accuracy_score(y_test, y_pred_test)
-test_preci = precision_score(y_test, y_pred_test)
-test_recall = recall_score(y_test, y_pred_test)
-test_f1 = f1_score(y_test, y_pred_test)
-print(f"Test Accuracy : {test_acc:.3f}")
-print("Test Confusion Matrix:\n", confusion_matrix(y_test, y_pred_test))
-print("Test Precision Score:\n", test_preci)
-print("Test Recall Score:\n", test_recall)
-print("Test F1-Score:\n", test_f1)
+w2v_model = Word2Vec.Word2Vec(vector_size=100, window=5, min_count=1, workers=4)
+w2v_model.build_vocab(X_train_tokens)
+w2v_model.train(X_train_tokens, total_examples=w2v_model.corpus_count, epochs=10)
+X_train_word2vec = np.array([get_avg_word2vec(tokens, w2v_model, 100) for tokens in X_train_tokens])
+X_test_word2vec = np.array([get_avg_word2vec(tokens, w2v_model, 100) for tokens in X_test_tokens])
